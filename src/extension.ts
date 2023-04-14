0d15c4550c478939fa13b33d32acdcfa214a04a1
Jinglei Ren <jinglei@merico.dev>
2023-04-14 09:29:10 +0800

    Add prompt command

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('devchat.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('devchat.prompt', () => {
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from DevChat!');
+		vscode.window.showInformationMessage('DevChat is under development!');
 	});
 
 	context.subscriptions.push(disposable);
