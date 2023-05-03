6ba0e00294e1afa4cb64a427cf6a4d9e3a10dd11
bobo.yang <bobo.yang@merico.dev>
2023-05-03 23:21:46 +0800

    add commit message command

--- src/commandManager.ts
+++ src/commandManager.ts
@@ -2,7 +2,7 @@ export interface Command {
     name: string;
     pattern: string;
     description: string;
-    handler: (userInput: string) => string;
+    handler: (userInput: string) => Promise<string>;
   }
   
   class CommandManager {
@@ -27,22 +27,35 @@ export interface Command {
       return this.commands;
     }
   
-    processText(text: string): string {
-      let result = text;
-  
-      this.commands.forEach((commandObj) => {
-        const commandPattern = new RegExp(
-          `\\/(${commandObj.pattern.replace("{{prompt}}", "(.+?)")})`,
-          "g"
-        );
-  
-        result = result.replace(commandPattern, (_, matchedUserInput) => {
-          return commandObj.handler(matchedUserInput);
-        });
-      });
-  
-      return result;
-    }
+    async processText(text: string): Promise<string> {
+        // 定义一个异步函数来处理单个命令
+        const processCommand = async (commandObj: Command, userInput: string) => {
+          const commandPattern = new RegExp(
+            `\\/(${commandObj.pattern.replace('{{prompt}}', '(.+?)')})`,
+            'g'
+          );
+          const matches = Array.from(text.matchAll(commandPattern));
+          const replacements = await Promise.all(
+            matches.map(async (match) => {
+              const matchedUserInput = match[1];
+              return await commandObj.handler(matchedUserInput);
+            })
+          );
+          let result = text;
+          for (let i = 0; i < matches.length; i++) {
+            result = result.replace(matches[i][0], replacements[i]);
+          }
+          return result;
+        };
+      
+        // 处理所有命令
+        let result = text;
+        for (const commandObj of this.commands) {
+          result = await processCommand(commandObj, result);
+        }
+      
+        return result;
+    }  
   }
   
   export default CommandManager;
--- src/exampleCommand2.ts
+++ src/exampleCommand2.ts
@@ -4,7 +4,7 @@ export const exampleCommand2: Command = {
   name: 'exampleCommand2',
   pattern: 'example: command2 {{prompt}}',
   description: '这是一个示例命令2',
-  handler: (userInput: string) => {
+  handler: async (userInput: string) => {
     return `示例命令2处理了以下文本：${userInput}`;
   },
 };
