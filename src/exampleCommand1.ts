05bee3880b92be3a5ca057447eef9683edebbcb3
bobo.yang <bobo.yang@merico.dev>
2023-05-03 23:21:46 +0800

    add commit message command

--- src/exampleCommand1.ts
+++ src/exampleCommand1.ts
@@ -4,7 +4,7 @@ export const exampleCommand1: Command = {
   name: 'exampleCommand1',
   pattern: 'example: command1 {{prompt}}',
   description: '这是一个示例命令1',
-  handler: (userInput: string) => {
+  handler: async (userInput: string) => {
     return `示例命令1处理了以下文本：${userInput}`;
   },
 };
