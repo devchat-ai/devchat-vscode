08be9a52dcc3dcc044c2e9d2c065ac754e7e87f2
bobo.yang <bobo.yang@merico.dev>
2023-05-09 20:46:57 +0800

    commitmsg Add configuration options for log count and skip
    - Added new configuration options in package.json for maxLogCount and logSkip
    - Updated historyMessages.ts to handle the new configuration options
    - Modified devchat.ts to use the new configuration options when fetching logs

--- src/handler/historyMessages.ts
+++ src/handler/historyMessages.ts
@@ -1,13 +1,55 @@
 import * as vscode from 'vscode';
-import DevChat, { LogOptions } from '../toolwrapper/devchat';
+import DevChat, { LogOptions, LogEntry } from '../toolwrapper/devchat';
 import { MessageHandler } from './messageHandler';
+import messageHistory from '../util/messageHistory';
+
+interface LoadHistoryMessages {
+	command: string;
+	entries: Array<LogEntry>;
+}
 
 export async function historyMessages(message: any, panel: vscode.WebviewPanel): Promise<void> {
 	const devChat = new DevChat();
 
 	const logOptions: LogOptions = message.options || {};
 	const logEntries = await devChat.log(logOptions);
-	MessageHandler.sendMessage(panel, { command: 'loadHistoryMessages', entries: logEntries });
+	
+	const logEntriesFlat = logEntries.flat();
+	// TODO handle context
+    
+	const logEntriesFlatFiltered = logEntriesFlat.map((entry) => {
+        return {
+			date: entry.date,
+			hash: entry.hash,
+			request: entry.request,
+			text: entry.response,
+			user: entry.user,
+			parent_hash: '',
+		};
+    });
+
+	for (let i = 0; i < logEntriesFlat.length; i++) {
+		let entryOld = logEntriesFlat[i];
+		let entryNew = {
+			date: entryOld.date,
+			hash: entryOld.hash,
+			request: entryOld.request,
+			text: entryOld.response,
+			user: entryOld.user,
+			parent_hash: '',
+		};
+		if (i > 0) {
+			entryNew.parent_hash = logEntriesFlat[i - 1].hash;
+		}
+		messageHistory.add(panel, entryNew);
+	}
+
+	const loadHistoryMessages: LoadHistoryMessages = {
+		command: 'loadHistoryMessages',
+		entries: logEntriesFlat,
+	};
+
+	MessageHandler.sendMessage(panel, loadHistoryMessages);
 	return;
 }
 
