0a8873333ce5008814338bbfb76358d68bd25658
bobo.yang <bobo.yang@merico.dev>
2023-05-18 08:57:13 +0800

    Add diff view and apply code changes functionality
    - Implement getDocumentText function to retrieve text from the editor
    - Update diffView function to handle temp files and diff view
    - Add getNewCode function to handle code changes
    - Implement applyCodeChanges and isValidActionString functions

--- src/handler/showDiff.ts
+++ src/handler/showDiff.ts
@@ -2,8 +2,34 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import { createTempSubdirectory } from '../util/commonUtil';
 import { regInMessage, regOutMessage } from '../util/reg_messages';
+import { applyCodeChanges, isValidActionString } from '../util/appyDiff';
+import { logger } from '../util/logger';
 
 
+async function getDocumentText(): Promise<{ text: string; beforSelect: string; select: string, afterSelect: string } | undefined> {
+	const validVisibleTextEditors = vscode.window.visibleTextEditors.filter(editor => editor.viewColumn !== undefined);
+
+	if (validVisibleTextEditors.length > 1) {
+        vscode.window.showErrorMessage(`There are more then one visible text editors. Please close all but one and try again.`);
+        return undefined;
+    }
+
+    const editor = validVisibleTextEditors[0];
+    if (!editor) {
+      return undefined;
+    }
+
+	// get whole text in editor
+	const text = editor.document.getText();
+
+	const selectedText = editor.document.getText(editor.selection);
+
+	const textBeforeSelection = editor.document.getText(new vscode.Range(new vscode.Position(0, 0), editor.selection.start));
+	const textAfterSelection = editor.document.getText(new vscode.Range(editor.selection.end, new vscode.Position(editor.document.lineCount, 0)));
+
+	// return multiple values
+	return { text, beforSelect: textBeforeSelection, select: selectedText, afterSelect: textAfterSelection };
+}
 
 export  async function diffView(code: string) {
     const validVisibleTextEditors = vscode.window.visibleTextEditors.filter(editor => editor.viewColumn !== undefined);
@@ -18,46 +44,61 @@ export  async function diffView(code: string) {
       return;
     }
 
-    const selectedText = editor.document.getText(editor.selection);
-
-    const curFile = editor.document.fileName;
-    
-    // get file name from fileSelected
+	const curFile = editor.document.fileName;
+	// get file name from fileSelected
     const fileName = path.basename(curFile);
 
-    // create temp directory and file
+	// create temp directory and file
     const tempDir = await createTempSubdirectory('devchat/context');
     const tempFile = path.join(tempDir, fileName);
 
     // save code to temp file
     await vscode.workspace.fs.writeFile(vscode.Uri.file(tempFile), Buffer.from(code));
 
-    if (selectedText) {
-      // create temp directory and file
-      const tempDir = await createTempSubdirectory('devchat/context');
-      const tempSelectCodeFile = path.join(tempDir, fileName);
+    // open diff view
+    vscode.commands.executeCommand('vscode.diff', vscode.Uri.file(tempFile), vscode.Uri.file(curFile), 'Diff View');
+}
 
-      // save code to temp file
-      await vscode.workspace.fs.writeFile(vscode.Uri.file(tempSelectCodeFile), Buffer.from(selectedText));
+async function getNewCode(message: any) : Promise<string | undefined> {
+	const codeTextObj = await getDocumentText();
+	if (!codeTextObj) {
+		logger.channel()!.error('getDocumentText failed');
+		return undefined;
+	}
 
-      // open diff view
-      vscode.commands.executeCommand('vscode.diff', vscode.Uri.file(tempSelectCodeFile), vscode.Uri.file(tempFile), 'Diff View');
-    } else {
-      // open diff view
-      vscode.commands.executeCommand('vscode.diff', vscode.Uri.file(curFile), vscode.Uri.file(tempFile), 'Diff View');
-    }
-  }
+	let newCode = message.content;
+	if (isValidActionString(message.content)) {
+		newCode = applyCodeChanges(codeTextObj.text, message.content);
+	} else {
+		// if select some text, then reconstruct the code
+		if (codeTextObj.select) {
+			newCode = codeTextObj.beforSelect + message.content + codeTextObj.afterSelect;
+		}
+	}
+
+	return newCode;
+}
 
 regInMessage({command: 'show_diff', content: ''});
 export async function showDiff(message: any, panel: vscode.WebviewPanel|vscode.WebviewView): Promise<void> {
-	diffView(message.content);
-    return;
+	const newCode = await getNewCode(message);	
+	if (!newCode) {
+		return;
+	}
+
+	diffView(newCode);
+	return;
 }
 
 regInMessage({command: 'block_apply', content: ''});
 export async function blockApply(message: any, panel: vscode.WebviewPanel|vscode.WebviewView): Promise<void> {
-	diffView(message.content);
-    return;
+	const newCode = await getNewCode(message);	
+	if (!newCode) {
+		return;
+	}
+
+	diffView(newCode);
+	return;
 }
 
 
---
+++ src/util/appyDiff.ts
@@ -0,0 +1,73 @@
+
+
+
+
+type Action = {
+	action: "delete" | "insert" | "modify";
+	content?: string;
+	insert_after?: string;
+	original_content?: string;
+	new_content?: string;
+};
+
+export function applyCodeChanges(originalCode: string, actionsString: string): string {
+    const actions = JSON.parse(actionsString) as Array<Action>;
+
+    const lines = originalCode.split('\n');
+
+    for (const action of actions) {
+        switch (action.action) {
+            case 'delete':
+                const deleteIndex = lines.findIndex(line => line.trim() === action.content);
+                if (deleteIndex !== -1) {
+                    lines.splice(deleteIndex, 1);
+                }
+                break;
+
+            case 'insert':
+                const insertIndex = lines.findIndex(line => line.trim() === action.insert_after);
+                if (insertIndex !== -1) {
+                    lines.splice(insertIndex + 1, 0, action.content!);
+                }
+                break;
+
+            case 'modify':
+                const modifyIndex = lines.findIndex(line => line.trim() === action.original_content);
+                if (modifyIndex !== -1) {
+                    lines[modifyIndex] = lines[modifyIndex].replace(lines[modifyIndex].trim(), action.new_content!);
+                }
+                break;
+        }
+    }
+
+    return lines.join('\n');
+}
+
+
+export function isValidActionString(actionString: string): boolean {
+  try {
+    const actions = JSON.parse(actionString) as Array<Action>;
+
+    for (const action of actions) {
+      if (!["delete", "insert", "modify"].includes(action.action)) {
+        return false;
+      }
+
+      if (action.action === "delete" && !action.content) {
+        return false;
+      }
+
+      if (action.action === "insert" && (!action.content || !action.insert_after)) {
+        return false;
+      }
+
+      if (action.action === "modify" && (!action.original_content || !action.new_content)) {
+        return false;
+      }
+    }
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
