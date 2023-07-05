b573dff2cf1c0d4346c8cf46ac069b3a19bcb01e
Rankin Zheng <runjin@merico.dev>
2023-07-06 06:22:04 +0800

    Enhance message copying and refilling in MessageContainer
    - Imported CopyButton and Icon360 from respective libraries for improved functionality.
    - Replaced the refill prompt icon with a 360 icon for better user understanding.
    - Added a copy message functionality for bot messages with a tooltip indicating the copy status.
    - Changed the state variable name from 'refilled' to 'done' for better context understanding.

--- src/views/MessageContainer.tsx
+++ src/views/MessageContainer.tsx
@@ -1,5 +1,5 @@
 
-import { Center, Text, Flex, Avatar, Accordion, Box, Stack, Container, Divider, ActionIcon, Tooltip } from "@mantine/core";
+import { Center, Text, Flex, Avatar, Accordion, Box, Stack, Container, Divider, ActionIcon, Tooltip, CopyButton } from "@mantine/core";
 import React from "react";
 import CodeBlock from "@/views/CodeBlock";
 
@@ -7,7 +7,7 @@ import CodeBlock from "@/views/CodeBlock";
 import SvgAvatarDevChat from '@/views/avatar_devchat.svg';
 // @ts-ignore
 import SvgAvatarUser from '@/views/avatar_spaceman.png';
-import { IconCheck, IconCopy } from "@tabler/icons-react";
+import { IconCheck, IconCopy, Icon360 } from "@tabler/icons-react";
 
 import { useAppDispatch, useAppSelector } from '@/views/hooks';
 import {
@@ -98,7 +98,7 @@ const MessageContext = (props: any) => {
 const MessageHeader = (props: any) => {
     const { type, message, contexts } = props;
     const dispatch = useAppDispatch();
-    const [refilled, setRefilled] = React.useState(false);
+    const [done, setDone] = React.useState(false);
     return (<Flex
         m='10px 0 10px 0'
         gap="sm"
@@ -121,20 +121,28 @@ const MessageHeader = (props: any) => {
         }
         <Text weight='bold'>{type === 'bot' ? 'DevChat' : 'User'}</Text>
         {type === 'user'
-            ? <Tooltip sx={{ padding: '3px', fontSize: 'var(--vscode-editor-font-size)' }} label={refilled ? 'Refilled' : 'Refill prompt'} withArrow position="left" color="gray">
+            ? <Tooltip sx={{ padding: '3px', fontSize: 'var(--vscode-editor-font-size)' }} label={done ? 'Refilled' : 'Refill prompt'} withArrow position="left" color="gray">
                 <ActionIcon size='sm' style={{ marginLeft: 'auto' }}
                     onClick={() => {
                         dispatch(setValue(message));
                         dispatch(setContexts(contexts));
-                        setRefilled(true);
-                        setTimeout(() => { setRefilled(false); }, 2000);
+                        setDone(true);
+                        setTimeout(() => { setDone(false); }, 2000);
                     }}>
-                    {refilled ? <IconCheck size="1rem" /> : <IconCopy size="1.125rem" />}
+                    {done ? <IconCheck size="1rem" /> : <Icon360 size="1.125rem" />}
                 </ActionIcon>
             </Tooltip>
-            : <></>
+            : <CopyButton value={message} timeout={2000}>
+                {({ copied, copy }) => (
+                    <Tooltip sx={{ padding: '3px', fontSize: 'var(--vscode-editor-font-size)' }} label={copied ? 'Copied' : 'Copy message'} withArrow position="left" color="gray">
+                        <ActionIcon size='xs' color={copied ? 'teal' : 'gray'} onClick={copy} style={{ marginLeft: 'auto' }}>
+                            {copied ? <IconCheck size="1rem" /> : <IconCopy size="1rem" />}
+                        </ActionIcon>
+                    </Tooltip>
+                )}
+            </CopyButton>
         }
-    </Flex>);
+    </Flex >);
 };
 
 const MessageContainer = (props: any) => {
