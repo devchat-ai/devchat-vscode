dc96991fca571b53bf02b4b53c7e3f6555f3caff
bobo.yang <bobo.yang@merico.dev>
2023-07-24 00:11:56 +0800

    Refactor symbol reference and definition handling
    - Extracted getSelectedSymbol function to improve code readability.
    - Fixed the scope of refLocations variable to avoid reference error.
    - Added error handling for vscode.executeReferenceProvider command.
    - Refactored the way symbol references are retrieved and handled.
    - Improved the logic for getting symbol definitions.

--- src/context/contextDefRefs.ts
+++ src/context/contextDefRefs.ts
@@ -7,93 +7,94 @@ import { createTempSubdirectory, git_ls_tree, runCommandStringAndWriteOutput } f
 
 import { logger } from '../util/logger';
 import { handleCodeSelected } from './contextCodeSelected';
+import { log } from 'console';
 
 
-export const defRefsContext: ChatContext = {
-  name: 'symbol references',
-  description: 'The context where the selected symbol is referenced',
-  handler: async () => {
+async function getSelectedSymbol(): Promise<vscode.DocumentSymbol | undefined> {
 	const activeEditor = vscode.window.activeTextEditor;
 	if (!activeEditor) {
-		return [];
+		return undefined;
 	}
 
 	const document = activeEditor.document;
 	const selection = activeEditor.selection;
-	const selectedText = document.getText(selection);
 
-	// if there are no text selected, then return
-	if (selectedText === "") {
-		logger.channel()?.warn(`No text has been selected!`);
-		return [];
+	const symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>('vscode.executeDocumentSymbolProvider', document.uri);
+	if (!symbols) {
+		return undefined;
 	}
 
+	let closestSymbol: vscode.DocumentSymbol | undefined = undefined;
+	let maxCloseness = -1;
 
-	const filesList = await git_ls_tree(true);
+	const checkSymbol = (symbol: vscode.DocumentSymbol) => {
+		if (symbol.range.start.isAfter(selection.end) || symbol.range.end.isBefore(selection.start)) {
+			return;
+		}
 
-	let symbolFileMap: Map<string, vscode.DocumentSymbol[]> = new Map();
-	for (const file of filesList) {
-        try {
-            const fileUri = vscode.Uri.file(file);
-            const symbolsT: vscode.DocumentSymbol[]  = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
-                'vscode.executeDocumentSymbolProvider',
-                fileUri
-            );
-            if (symbolsT) {
-				symbolFileMap[file] = symbolsT;
-            }
-        } catch (e) {
-            logger.channel()?.error(`Error: ${e}`);
-        }
-    }
+		const intersection = Math.max(-2, Math.min(selection.end.line, symbol.range.end.line) - Math.max(selection.start.line, symbol.range.start.line) + 1);
+		const closeness = intersection / Math.max(selection.end.line - selection.start.line + 1, symbol.range.end.line - symbol.range.start.line + 1);
+		if (closeness > maxCloseness) {
+			maxCloseness = closeness;
+			closestSymbol = symbol;
+		}
 
+		for (const child of symbol.children) {
+			checkSymbol(child);
+		}
+	};
 
-	let contextList: string[] = [];
-	const refLocations = await vscode.commands.executeCommand<vscode.Location[]>(
-		'vscode.executeReferenceProvider',
-		document.uri,
-		selection.start
-	);
+	for (const symbol of symbols) {
+		checkSymbol(symbol);
+	}
 
-	// symbol set 
-	let symbolMap: Map<String, vscode.DocumentSymbol> = new Map();
+	return closestSymbol;
+}
 
-	if (refLocations) {
-		// find symbol include refLocation symbol
-		for (const refLocation of refLocations) {
-			const refLocationFile = refLocation.uri.fsPath;
-			// if symbolFileMap has refLocationFile, then find symbol include refLocation symbol
-			if (symbolFileMap[refLocationFile]) {
-				let existInSymbol = false;
-				for (const symbol of symbolFileMap[refLocationFile]) {
-					if (refLocation.range.start.isAfterOrEqual(symbol.range.start) && refLocation.range.end.isBeforeOrEqual(symbol.range.end)) {
-						const symbolKey = `${refLocationFile} ${symbol.name} ${symbol.kind} ${symbol.range.start.line}:${symbol.range.start.character}-${symbol.range.end.line}:${symbol.range.end.character}`;
-						// if symbolKey not in symbolMap, then add it to symbolMap
-						if (!symbolMap[symbolKey]) {
-							existInSymbol = true;
-							symbolMap[symbolKey] = symbol;
-							const documentNew = await vscode.workspace.openTextDocument(refLocationFile);
+export const defRefsContext: ChatContext = {
+	name: 'symbol references',
+	description: 'The context where the selected symbol is referenced',
+	handler: async () => {
+		const activeEditor = vscode.window.activeTextEditor;
+		if (!activeEditor) {
+			return [];
+		}
 
-							if (symbol.kind === vscode.SymbolKind.Variable) {
-								const renageNew = new vscode.Range(symbol.range.start.line, 0, symbol.range.end.line, 10000);
-								contextList.push(await handleCodeSelected(refLocationFile, documentNew.getText(renageNew)));
-							} else {
-								contextList.push(await handleCodeSelected(refLocationFile, documentNew.getText(symbol.range)));
-							}
-							break;
-						}
-					}
-				}
-				
-				if (existInSymbol === false) {
-					const documentNew = await vscode.workspace.openTextDocument(refLocationFile);
-					const renageNew = new vscode.Range(refLocation.range.start.line, 0, refLocation.range.end.line, 10000);
-					contextList.push(await handleCodeSelected(refLocationFile, documentNew.getText(renageNew)));
-				}
+		const document = activeEditor.document;
+
+		// get all references of selected symbol define
+		const selectedSymbol = await getSelectedSymbol();
+		if (!selectedSymbol) {
+			logger.channel()?.error(`Error: no matched symbol found for selected text!`);
+			logger.channel()?.show();
+			return [];
+		}
+		logger.channel()?.info(`selectedSymbol: ${selectedSymbol.name} ${selectedSymbol.kind} ${selectedSymbol.range.start.line} ${selectedSymbol.range.end.line}`)
+
+		// 获取selectedSymbol的引用信息
+		let contextList: string[] = [];
+		let refLocations;
+		try {
+			refLocations = await vscode.commands.executeCommand<vscode.Location[]>(
+				'vscode.executeReferenceProvider',
+				document.uri,
+				selectedSymbol.selectionRange.start
+			);
+		} catch (error) {
+			logger.channel()?.error(`secretStorageGet error: ${error}`);
+			return [];
+		}
+
+		if (refLocations) {
+			// find symbol include refLocation symbol
+			for (const refLocation of refLocations) {
+				const refLocationFile = refLocation.uri.fsPath;
+				const documentNew = await vscode.workspace.openTextDocument(refLocationFile);
+
+				const renageNew = new vscode.Range(refLocation.range.start.line - 2, 0, refLocation.range.end.line + 2, 10000);
+				contextList.push(await handleCodeSelected(refLocationFile, documentNew.getText(renageNew)));
 			}
 		}
+		return contextList;
 	}
-
-	return contextList;
-  },
 };
--- src/context/contextRefDefs.ts
+++ src/context/contextRefDefs.ts
@@ -7,77 +7,158 @@ import { createTempSubdirectory, git_ls_tree, runCommandStringAndWriteOutput } f
 
 import { logger } from '../util/logger';
 import { handleCodeSelected } from './contextCodeSelected';
+import DevChat, { ChatOptions } from '../toolwrapper/devchat';
 
 
-export const refDefsContext: ChatContext = {
-  name: 'symbol definitions',
-  description: 'Context of symbol definition in selected text',
-  handler: async () => {
+async function getCurrentSelectText(): Promise<string> {
 	const activeEditor = vscode.window.activeTextEditor;
 	if (!activeEditor) {
-		return [];
+		return "";
 	}
 
 	const document = activeEditor.document;
 	const selection = activeEditor.selection;
 	const selectedText = document.getText(selection);
-	
-	// if there are no text selected, then return
-	if (selectedText === "") {
-		logger.channel()?.warn(`No text has been selected!`);
+
+	return selectedText;
+}
+
+async function getUndefinedSymbols(content: string): Promise<string[]> {
+	// run devchat prompt command
+	const devChat = new DevChat();
+	const chatOptions: ChatOptions = {};
+
+	const onData = (partialResponse) => { };
+	const newContent = content + `\n只列出当前代码中缺少定义的符号列表，不需要其他处理。应答形式为markdown code block，例如：
+	\`\`\`json
+	["f1", "f2"]
+	\`\`\`
+	不能调用GPT function.`;
+
+	const chatResponse = await devChat.chat(newContent, chatOptions, onData);
+	if (!chatResponse.isError) {
+		await devChat.delete(chatResponse['prompt-hash']);
+	}
+
+	// parse data in chatResponse.response
+	// data format as:
+	// ```json {data} ```
+	// so, parse data between ```json and ```
+	if (!chatResponse || !chatResponse.response) {
+		return [];
+	}
+	if (!chatResponse.response.match(/```json([\s\S]*)```/)) {
 		return [];
 	}
 
-	const filesList = await git_ls_tree(true);
+	logger.channel()?.info(`getUndefinedSymbols: ${chatResponse.response}`);
+	const responseText = chatResponse.response.match(/```json([\s\S]*)```/)![1];
+	// parse responseText to json
+	return JSON.parse(responseText);
+}
 
-	let contextList: string[] = [];
-	for (const file of filesList) {
-        try {
-            const fileUri = vscode.Uri.file(file);
-            const symbolsT: vscode.DocumentSymbol[]  = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
-                'vscode.executeDocumentSymbolProvider',
-                fileUri
-            );
-            if (symbolsT) {
-				symbolsT.forEach(symbol => {
-					logger.channel()?.info(`Symbol X1: ${symbol.name}, Kind: ${symbol.kind}, Range: ${symbol.range.start.line}:${symbol.range.start.character}-${symbol.range.end.line}:${symbol.range.end.character}`);
-				});
+async function getSymbolDefine(symbolList: string[]): Promise<string[]> {
+	const activeEditor = vscode.window.activeTextEditor;
+	const document = activeEditor!.document;
+	const selection = activeEditor!.selection;
+	const selectedText = document.getText(selection);
 
-				for (const symbol of symbolsT) {
-					const symbolRange = symbol.selectionRange;
+	let contextList: string[] = [await handleCodeSelected(activeEditor!.document.uri.fsPath, selectedText)];
+	let hasVisitedSymbols: Set<string> = new Set();
 
-					const refLocations = await vscode.commands.executeCommand<vscode.Location[]>(
-						'vscode.executeReferenceProvider',
-						fileUri,
-						symbolRange.start
+	// visit each symbol in symbolList, and get it's define
+	for (const symbol of symbolList) {
+		// get symbol position in selectedText
+		// if selectedText is "abc2+abc", symbol is "abc", then symbolPosition is 5 not 0
+		// because abc2 is not a symbol
+		const escapedSymbol = symbol.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+		// Create a RegExp with the escaped symbol and word boundaries
+		const regex = new RegExp(`\\b${escapedSymbol}\\b`, 'gu');
+
+		// Find the match in the selected text
+		const matches = [...selectedText.matchAll(regex)];
+
+		// If the symbol is found
+		if (matches.length === 0) {
+			continue;
+		}
+
+		for (const match of matches) {
+			const symbolPosition = match.index!;
+
+			// if symbol is like a.b.c, then split it to a, b, c
+			const symbolSplit = symbol.split(".");
+			let curPosition = 0;
+			for (const symbolSplitItem of symbolSplit) {
+				const symbolPositionNew = symbol.indexOf(symbolSplitItem, curPosition) + symbolPosition;
+				curPosition = symbolPositionNew - symbolPosition + symbolSplitItem.length;
+
+				// call vscode.executeDefinitionProvider
+				const refLocations = await vscode.commands.executeCommand<vscode.Location[]>(
+					'vscode.executeDefinitionProvider',
+					document.uri,
+					selection.start.translate({ characterDelta: symbolPositionNew })
+				);
+
+				// visit each refLocation, and get it's define
+				for (const refLocation of refLocations) {
+					const refLocationString = refLocation.uri.fsPath + "-" + refLocation.range.start.line + ":" + refLocation.range.start.character + "-" + refLocation.range.end.line + ":" + refLocation.range.end.character;
+					if (hasVisitedSymbols.has(refLocationString)) {
+						continue;
+					}
+					hasVisitedSymbols.add(refLocationString);
+
+					// get defines in refLocation file
+					const symbolsT: vscode.DocumentSymbol[] = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>(
+						'vscode.executeDocumentSymbolProvider',
+						refLocation.uri
 					);
 
-					if (refLocations) {
-						// 判断是否有引用处于selection范围内
-						let existRef = false;
-						for (const refLocation of refLocations) {
-							if (refLocation.uri.fsPath === document.uri.fsPath && refLocation.range.start.isAfterOrEqual(selection.start) && refLocation.range.end.isBeforeOrEqual(selection.end)) {
-								existRef = true;
-								break;
-							}
+					let targetSymbol: any = undefined;
+					const visitFun = (symbol: vscode.DocumentSymbol) => {
+						if (refLocation.range.start.isAfterOrEqual(symbol.range.start) && refLocation.range.end.isBeforeOrEqual(symbol.range.end)) {
+							targetSymbol = symbol;
 						}
-						if (existRef) {
-							const documentNew = await vscode.workspace.openTextDocument(fileUri);
-							if (symbol.kind === vscode.SymbolKind.Variable) {
-								const renageNew = new vscode.Range(symbol.range.start.line, 0, symbol.range.end.line, 10000);
-								contextList.push(await handleCodeSelected(file, documentNew.getText(renageNew)));
-							} else {
-								contextList.push(await handleCodeSelected(file, documentNew.getText(symbol.range)));
-							}
+
+						if (refLocation.range.start.isAfter(symbol.range.end) || refLocation.range.end.isBefore(symbol.range.start)) {
+							return;
+						}
+
+						for (const child of symbol.children) {
+							visitFun(child);
+						}
+					}
+					for (const symbol of symbolsT) {
+						visitFun(symbol);
+					}
+
+					if (targetSymbol !== undefined) {
+						const documentNew = await vscode.workspace.openTextDocument(refLocation.uri);
+
+						if (targetSymbol.kind === vscode.SymbolKind.Variable) {
+							const renageNew = new vscode.Range(targetSymbol.range.start.line, 0, targetSymbol.range.end.line, 10000);
+							contextList.push(await handleCodeSelected(refLocation.uri.fsPath, documentNew.getText(renageNew)));
+						} else {
+							contextList.push(await handleCodeSelected(refLocation.uri.fsPath, documentNew.getText(targetSymbol.range)));
 						}
 					}
 				}
-            }
-        } catch (e) {
-            logger.channel()?.error(`Error: ${e}`);
-        }
-    }
-
+			}
+		}
+	}
 	return contextList;
-  },
+}
+
+
+export const refDefsContext: ChatContext = {
+	name: 'symbol definitions',
+	description: 'Context of symbol definition in selected text',
+	handler: async () => {
+		const selectedText = await getCurrentSelectText();
+		const symbolList = await getUndefinedSymbols(selectedText);
+		const contextList = await getSymbolDefine(symbolList);
+
+		return contextList;
+	},
 };
