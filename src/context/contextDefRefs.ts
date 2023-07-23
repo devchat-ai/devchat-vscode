5cf0828a93c7a962a5383826012350771ad903e6
bobo.yang <bobo.yang@merico.dev>
2023-07-24 00:11:56 +0800

    Refine context descriptions and add checks for selected text
    - Updated descriptions for 'symbol references' and 'symbol definitions' contexts for clarity.
    - Added checks to return early if no text has been selected in both contexts.
    - Removed unnecessary logging of selected text and files list.
    - Added logic to handle cases where symbol kind is a variable.
    - Removed redundant logging of reference URI and range.

--- src/context/contextDefRefs.ts
+++ src/context/contextDefRefs.ts
@@ -11,7 +11,7 @@ import { handleCodeSelected } from './contextCodeSelected';
 
 export const defRefsContext: ChatContext = {
   name: 'symbol references',
-  description: 'The context where the symbol is referenced',
+  description: 'The context where the selected symbol is referenced',
   handler: async () => {
 	const activeEditor = vscode.window.activeTextEditor;
 	if (!activeEditor) {
@@ -21,11 +21,15 @@ export const defRefsContext: ChatContext = {
 	const document = activeEditor.document;
 	const selection = activeEditor.selection;
 	const selectedText = document.getText(selection);
-	logger.channel()?.info(`selected text: ${selectedText}`);
+
+	// if there are no text selected, then return
+	if (selectedText === "") {
+		logger.channel()?.warn(`No text has been selected!`);
+		return [];
+	}
 
 
 	const filesList = await git_ls_tree(true);
-	logger.channel()?.info(`filesList: ${filesList}`);
 
 	let symbolFileMap: Map<string, vscode.DocumentSymbol[]> = new Map();
 	for (const file of filesList) {
@@ -60,16 +64,31 @@ export const defRefsContext: ChatContext = {
 			const refLocationFile = refLocation.uri.fsPath;
 			// if symbolFileMap has refLocationFile, then find symbol include refLocation symbol
 			if (symbolFileMap[refLocationFile]) {
+				let existInSymbol = false;
 				for (const symbol of symbolFileMap[refLocationFile]) {
 					if (refLocation.range.start.isAfterOrEqual(symbol.range.start) && refLocation.range.end.isBeforeOrEqual(symbol.range.end)) {
 						const symbolKey = `${refLocationFile} ${symbol.name} ${symbol.kind} ${symbol.range.start.line}:${symbol.range.start.character}-${symbol.range.end.line}:${symbol.range.end.character}`;
 						// if symbolKey not in symbolMap, then add it to symbolMap
 						if (!symbolMap[symbolKey]) {
+							existInSymbol = true;
 							symbolMap[symbolKey] = symbol;
 							const documentNew = await vscode.workspace.openTextDocument(refLocationFile);
-							contextList.push(await handleCodeSelected(refLocationFile, documentNew.getText(symbol.range)));
+
+							if (symbol.kind === vscode.SymbolKind.Variable) {
+								const renageNew = new vscode.Range(symbol.range.start.line, 0, symbol.range.end.line, 10000);
+								contextList.push(await handleCodeSelected(refLocationFile, documentNew.getText(renageNew)));
+							} else {
+								contextList.push(await handleCodeSelected(refLocationFile, documentNew.getText(symbol.range)));
+							}
+							break;
 						}
 					}
+				}
+				
+				if (existInSymbol === false) {
+					const documentNew = await vscode.workspace.openTextDocument(refLocationFile);
+					const renageNew = new vscode.Range(refLocation.range.start.line, 0, refLocation.range.end.line, 10000);
+					contextList.push(await handleCodeSelected(refLocationFile, documentNew.getText(renageNew)));
 				}
 			}
 		}
--- src/context/contextRefDefs.ts
+++ src/context/contextRefDefs.ts
@@ -11,7 +11,7 @@ import { handleCodeSelected } from './contextCodeSelected';
 
 export const refDefsContext: ChatContext = {
   name: 'symbol definitions',
-  description: 'The context of the relevant definition',
+  description: 'Context of symbol definition in selected text',
   handler: async () => {
 	const activeEditor = vscode.window.activeTextEditor;
 	if (!activeEditor) {
@@ -21,10 +21,14 @@ export const refDefsContext: ChatContext = {
 	const document = activeEditor.document;
 	const selection = activeEditor.selection;
 	const selectedText = document.getText(selection);
-	logger.channel()?.info(`selected text: ${selectedText}`);
+	
+	// if there are no text selected, then return
+	if (selectedText === "") {
+		logger.channel()?.warn(`No text has been selected!`);
+		return [];
+	}
 
 	const filesList = await git_ls_tree(true);
-	logger.channel()?.info(`filesList: ${filesList}`);
 
 	let contextList: string[] = [];
 	for (const file of filesList) {
@@ -51,9 +55,6 @@ export const refDefsContext: ChatContext = {
 					if (refLocations) {
 						// 判断是否有引用处于selection范围内
 						let existRef = false;
-						refLocations.forEach((refLocation) => {
-							logger.channel()?.info(`Ref URI: ${refLocation.uri.fsPath}, Range: ${refLocation.range.start.line}:${refLocation.range.start.character}-${refLocation.range.end.line}:${refLocation.range.end.character}`);
-						});
 						for (const refLocation of refLocations) {
 							if (refLocation.uri.fsPath === document.uri.fsPath && refLocation.range.start.isAfterOrEqual(selection.start) && refLocation.range.end.isBeforeOrEqual(selection.end)) {
 								existRef = true;
@@ -61,10 +62,13 @@ export const refDefsContext: ChatContext = {
 							}
 						}
 						if (existRef) {
-							logger.channel()?.info(`SymbolT: ${symbol.name}, Kind: ${symbol.kind}, Range: ${symbol.range.start.line}:${symbol.range.start.character}-${symbol.range.end.line}:${symbol.range.end.character}`);
-							
 							const documentNew = await vscode.workspace.openTextDocument(fileUri);
-							contextList.push(await handleCodeSelected(file, documentNew.getText(symbol.range)));
+							if (symbol.kind === vscode.SymbolKind.Variable) {
+								const renageNew = new vscode.Range(symbol.range.start.line, 0, symbol.range.end.line, 10000);
+								contextList.push(await handleCodeSelected(file, documentNew.getText(renageNew)));
+							} else {
+								contextList.push(await handleCodeSelected(file, documentNew.getText(symbol.range)));
+							}
 						}
 					}
 				}
