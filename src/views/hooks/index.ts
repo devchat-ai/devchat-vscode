d92339e1b32f89d7fd2d38558a532ccf530ca46a
Rankin Zheng <runjin@merico.dev>
2023-08-18 00:21:44 +0800

    Remove Redux and related slices
    - Deleted Redux store configuration file (store.ts).
    - Removed inputSlice.ts and chatSlice.ts which contained Redux slices for managing chat and input state.
    - Deleted index.ts which contained Redux hooks for dispatching actions and selecting state.

--- src/views/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from '@/views/reducers/store';
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
