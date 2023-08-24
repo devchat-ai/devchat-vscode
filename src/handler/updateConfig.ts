53fb5ff12b19af006a25ec5912c0dbd7d482a7b4
bobo.yang <bobo.yang@merico.dev>
2023-08-24 10:24:28 +0800

    Update getSetting handler to send configuration value
    - Imported MessageHandler in updateConfig.ts.
    - Updated getSetting function to send the fetched configuration value using MessageHandler.
    - Registered an outgoing message for 'getSetting' command.

--- src/handler/updateConfig.ts
+++ src/handler/updateConfig.ts
@@ -4,8 +4,8 @@ Update config
 
 import * as vscode from 'vscode';
 import { regInMessage, regOutMessage } from '../util/reg_messages';
-import { logger } from '../util/logger';
 import { UiUtilWrapper } from '../util/uiUtil';
+import { MessageHandler } from './messageHandler';
 
 regInMessage({command: 'updateSetting', key1: "DevChat", key2: "OpenAI", value:"xxxx"});
 export async function updateSetting(message: any, panel: vscode.WebviewPanel|vscode.WebviewView): Promise<void> {
@@ -13,6 +13,7 @@ export async function updateSetting(message: any, panel: vscode.WebviewPanel|vsc
 }
 
 regInMessage({command: 'getSetting', key1: "DevChat", key2: "OpenAI"});
+regOutMessage({command: 'getSetting', value: "GPT-4"});
 export async function getSetting(message: any, panel: vscode.WebviewPanel|vscode.WebviewView): Promise<void> {
-    UiUtilWrapper.getConfiguration(message.key1, message.key2);
+    MessageHandler.sendMessage(panel, {"command": "getSetting", "value": UiUtilWrapper.getConfiguration(message.key1, message.key2)});
 }
