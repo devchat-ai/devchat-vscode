5bcd525b9af3ab2a20aebfd0aff9295abed6e4eb
smallstone <>
2023-08-31 16:28:08 +0800

    feat: add wechat tip

--- src/views/components/WechatTip/index.tsx
+++ src/views/components/WechatTip/index.tsx
@@ -1,44 +1,99 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import messageUtil from "@/util/MessageUtil";
+import { IconCoin } from "@tabler/icons-react";
+import { HoverCard, Text, ActionIcon, Group } from "@mantine/core";
 
-const key =
-  "DC.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfaWQiOjgxODg1ODM1MDgyLCJqdGkiOjcyNjk5NTE1NzkyOTU5ODgzNjB9.s1P6Br4Cw3xki_76bqJ-HRr229Q0WjMQrQ3z7hXTPtk";
+const currencyMap = {
+  USD: "$",
+  RMB: "¥",
+};
+
+function formatBalance(balance: number) {
+  return Math.round(balance * 100) / 100;
+}
+
+function formatCurrency(balance: number, currency: string) {
+  return `${currencyMap[currency] || currency} ${balance}`;
+}
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export default function WechatTip() {
+  const [bindWechat, setBindWechat] = useState(false);
+  const [link, setLink] = useState("https://devchat.ai");
+  const [balance, setBalance] = useState(0);
+  const [currency, setCurrency] = useState("USD");
+
   const getSettings = () => {
     messageUtil.sendMessage({
-      command: "getSetting",
-      key1: "DevChat",
-      key2: "API_ENDPOINT",
+      command: "getUserAccessKey",
     });
   };
   useEffect(() => {
     getSettings();
-    messageUtil.registerHandler("getSetting", (message: { value: string }) => {
-      console.log("value API_ENDPOINT: ", message);
-    });
-
-    axios
-      .get("https://apptest.devchat.ai/api/v1/users/profile", {
-        headers: { Authorization: `Bearer ${key}` },
-      })
-      .then((res) => {
-        console.log("res: ", res);
-      });
+    messageUtil.registerHandler(
+      "getUserAccessKey",
+      (message: { endPoint: string; accessKey: string; keyType: string }) => {
+        if (message.keyType === "DevChat" && message.accessKey) {
+          let url = "https://app.devchat.ai";
+          if (message.endPoint.includes("apptest.devchat.ai")) {
+            url = "https://apptest.devchat.ai";
+            setLink("https://test.devchat.ai");
+          }
+          axios
+            .get(`${url}/api/v1/users/profile`, {
+              headers: { Authorization: `Bearer ${message.accessKey}` },
+            })
+            .then((res) => {
+              if (res?.data?.user?.wechat_nickname) {
+                setBindWechat(true);
+              }
+              if (res?.data?.organization?.balance) {
+                setBalance(formatBalance(res?.data?.organization?.balance));
+                setCurrency(res?.data?.organization?.currency);
+              }
+            });
+        }
+      }
+    );
   }, []);
 
   return (
     <div
       style={{
-        padding: "10px 15px",
-        backgroundColor: "var(--vscode-input-background)",
         position: "absolute",
+        zIndex: 2,
+        right: 5,
+        top: 5,
       }}
     >
-      Your remaining credit is ¥1.5. Sign in to{" "}
-      <a href="https://test.devchat.ai">devchat.ai </a>to earn additional ¥8.
+      <HoverCard width="240">
+        <HoverCard.Target>
+          <ActionIcon
+            color="blue"
+            radius="xl"
+            variant="filled"
+            sx={{
+              opacity: 0.5,
+              transition: "opacity 300ms ease",
+              "&:hover": {
+                opacity: 1,
+              },
+            }}
+          >
+            <IconCoin />
+          </ActionIcon>
+        </HoverCard.Target>
+        <HoverCard.Dropdown>
+          <Group style={{ width: "90%" }}>
+            <Text size="sm">
+              Your remaining credit is {formatCurrency(balance, currency)}. Sign
+              in to <a href={link}>devchat.ai </a>to{" "}
+              {bindWechat ? "purchase more tokens." : "earn additional ¥8"}
+            </Text>
+          </Group>
+        </HoverCard.Dropdown>
+      </HoverCard>
     </div>
   );
 }
