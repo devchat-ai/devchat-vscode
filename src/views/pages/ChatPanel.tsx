b5ab675d80b0fe6fdc783a6b5833b5a05064518d
Rankin Zheng <runjin@merico.dev>
2023-09-13 20:47:22 +0800

    Refactor ChatPanel component
    - Import necessary components from @mantine/core and @tabler/icons-react.
    - Remove unused imports and dependencies.
    - Update the returned JSX structure and layout.
    - Adjust styling and spacing of components.
    - Remove unused code and dependencies.

--- src/views/pages/ChatPanel.tsx
+++ src/views/pages/ChatPanel.tsx
@@ -1,24 +1,16 @@
 import * as React from "react";
 import { useEffect, useRef } from "react";
 import {
-  ActionIcon,
-  Alert,
-  Anchor,
-  Box,
-  Button,
-  Center,
-  Chip,
-  Container,
-  Flex,
-  Group,
-  Radio,
-  Stack,
-  px,
+    ActionIcon,
+    Alert,
+    Box,
+    Button,
+    Center,
+    Stack,
 } from "@mantine/core";
 import { ScrollArea } from "@mantine/core";
-import { useResizeObserver, useTimeout, useViewportSize } from "@mantine/hooks";
+import { useElementSize, useResizeObserver, useTimeout, useViewportSize } from "@mantine/hooks";
 import messageUtil from "@/util/MessageUtil";
-import CurrentMessage from "@/views/components/CurrentMessage";
 import StopButton from "@/views/components/StopButton";
 import RegenerationButton from "@/views/components/RegenerationButton";
 import { observer } from "mobx-react-lite";
@@ -28,243 +20,208 @@ import { Message } from "@/views/stores/ChatStore";
 import InputMessage from "@/views/components/InputMessage";
 import MessageList from "@/views/components/MessageList";
 import {
-  IconCircleArrowDownFilled,
-  IconExternalLink,
+    IconCircleArrowDownFilled,
+    IconExternalLink,
 } from "@tabler/icons-react";
 
 const chatPanel = observer(() => {
-  const { input, chat } = useMst();
+    const { input, chat } = useMst();
 
-  const [chatContainerRef, chatContainerRect] = useResizeObserver();
-  const scrollViewport = useRef<HTMLDivElement>(null);
-  const { height, width } = useViewportSize();
+    const [chatContainerRef, chatContainerRect] = useResizeObserver();
+    const scrollViewport = useRef<HTMLDivElement>(null);
+    const { height, width } = useViewportSize();
+    const { ref:inputAreatRef, height:inputAreaHeight } = useElementSize();
 
-  const chatPanelWidth = chatContainerRect.width;
 
-  const scrollToBottom = () =>
-    scrollViewport?.current?.scrollTo({
-      top: scrollViewport.current.scrollHeight,
-      behavior: "smooth",
-    });
+    const chatPanelWidth = chatContainerRect.width;
 
-  const getSettings = () => {
-    messageUtil.sendMessage({
-      command: "getSetting",
-      key1: "devchat",
-      key2: "defaultModel",
-    });
-  };
-
-  const getFeatureToggles = () => {
-    messageUtil.sendMessage({
-      command: "featureToggles",
-    });
-  };
-
-  const timer = useTimeout(() => {
-    if (chat.isBottom) {
-      scrollToBottom();
-    }
-  }, 1000);
-
-  const chipStyle = {
-    color: "var(--vscode-checkbox-foreground)",
-    fontSize: "var(--vscode-editor-font-size)",
-    backgroundColor: "var(--vscode-checkbox-background)",
-    borderColor: "var(--vscode-checkbox-border)",
-    "&[data-checked]": {
-      borderColor: "var(--vscode-checkbox-selectBorder)",
-    },
-  };
-
-  const onScrollPositionChange = ({ x, y }) => {
-    const sh = scrollViewport.current?.scrollHeight || 0;
-    const vh = scrollViewport.current?.clientHeight || 0;
-    const gap = sh - vh - y;
-    const isBottom = sh < vh ? true : gap < 100;
-    const isTop = y === 0;
-    // console.log(`sh:${sh},vh:${vh},x:${x},y:${y},gap:${gap}`);
-    if (isBottom) {
-      chat.onMessagesBottom();
-    } else if (isTop) {
-      chat.onMessagesTop();
-      if (!chat.isLastPage) {
-        //TODO: Data loading flickers and has poor performance, so I temporarily disabled the loading logic.
-        // dispatch(fetchHistoryMessages({ pageIndex: pageIndex + 1 }));
-      }
-    } else {
-      chat.onMessagesMiddle();
-    }
-  };
-
-  useEffect(() => {
-    getSettings();
-    getFeatureToggles();
-    chat.fetchHistoryMessages({ pageIndex: 0 }).then();
-    messageUtil.registerHandler(
-      "receiveMessagePartial",
-      (message: { text: string }) => {
-        chat.startResponsing(message.text);
-        timer.start();
-      }
-    );
-    messageUtil.registerHandler(
-      "receiveMessage",
-      (message: { text: string; isError: boolean; hash }) => {
-        chat.stopGenerating(true, message.hash, message.text);
-        if (message.isError) {
-          chat.happendError(message.text);
-        }
-      }
-    );
-    messageUtil.registerHandler(
-      "systemMessage",
-      (message: { text: string }) => {
-        const messageItem = Message.create({
-          type: "system",
-          message: message.text,
+    const scrollToBottom = () =>
+        scrollViewport?.current?.scrollTo({
+            top: scrollViewport.current.scrollHeight,
+            behavior: "smooth",
         });
-        chat.newMessage(messageItem);
-        // start generating
-        chat.startSystemMessage();
-        // Clear the input field
-        input.setValue("");
-        input.clearContexts();
-      }
-    );
-    messageUtil.registerHandler("getSetting", (message: { value: string }) => {
-      chat.changeChatModel(message.value);
-    });
-    messageUtil.registerHandler(
-      "featureToggles",
-      (message: { features: object }) => {
-        // chat.changeChatModel(message.value);
-        chat.updateFeatures(message.features);
-      }
-    );
 
-    timer.start();
-    return () => {
-      timer.clear();
+    const getSettings = () => {
+        messageUtil.sendMessage({
+            command: "getSetting",
+            key1: "devchat",
+            key2: "defaultModel",
+        });
     };
-  }, []);
 
-  useEffect(() => {
-    scrollToBottom();
-  }, [chat.scrollBottom]);
+    const getFeatureToggles = () => {
+        messageUtil.sendMessage({
+            command: "featureToggles",
+        });
+    };
 
-  return (
-    <Box
-      ref={chatContainerRef}
-      miw={300}
-      sx={{
-        height: "100%",
-        margin: 0,
-        padding: "10px 10px 5px 10px",
-        background: "var(--vscode-sideBar-background)",
-        color: "var(--vscode-editor-foreground)",
-      }}
-    >
-      {!chat.isBottom && (
-        <ActionIcon
-          onClick={() => {
+    const timer = useTimeout(() => {
+        if (chat.isBottom) {
             scrollToBottom();
-          }}
-          title="Bottom"
-          variant="transparent"
-          sx={{ position: "absolute", bottom: 80, right: 20, zIndex: 1 }}
+        }
+    }, 1000);
+  
+    const onScrollPositionChange = ({ x, y }) => {
+        const sh = scrollViewport.current?.scrollHeight || 0;
+        const vh = scrollViewport.current?.clientHeight || 0;
+        const gap = sh - vh - y;
+        const isBottom = sh < vh ? true : gap < 100;
+        const isTop = y === 0;
+        // console.log(`sh:${sh},vh:${vh},x:${x},y:${y},gap:${gap}`);
+        if (isBottom) {
+            chat.onMessagesBottom();
+        } else if (isTop) {
+            chat.onMessagesTop();
+            if (!chat.isLastPage) {
+                //TODO: Data loading flickers and has poor performance, so I temporarily disabled the loading logic.
+                // dispatch(fetchHistoryMessages({ pageIndex: pageIndex + 1 }));
+            }
+        } else {
+            chat.onMessagesMiddle();
+        }
+    };
+
+    useEffect(() => {
+        getSettings();
+        getFeatureToggles();
+        chat.fetchHistoryMessages({ pageIndex: 0 }).then();
+        messageUtil.registerHandler(
+            "receiveMessagePartial",
+            (message: { text: string }) => {
+                chat.startResponsing(message.text);
+                timer.start();
+            }
+        );
+        messageUtil.registerHandler(
+            "receiveMessage",
+            (message: { text: string; isError: boolean; hash }) => {
+                chat.stopGenerating(true, message.hash, message.text);
+                if (message.isError) {
+                    chat.happendError(message.text);
+                }
+            }
+        );
+        messageUtil.registerHandler(
+            "systemMessage",
+            (message: { text: string }) => {
+                const messageItem = Message.create({
+                    type: "system",
+                    message: message.text,
+                });
+                chat.newMessage(messageItem);
+                // start generating
+                chat.startSystemMessage();
+                // Clear the input field
+                input.setValue("");
+                input.clearContexts();
+            }
+        );
+        messageUtil.registerHandler("getSetting", (message: { value: string }) => {
+            chat.changeChatModel(message.value);
+        });
+        messageUtil.registerHandler(
+            "featureToggles",
+            (message: { features: object }) => {
+                // chat.changeChatModel(message.value);
+                chat.updateFeatures(message.features);
+            }
+        );
+
+        timer.start();
+        return () => {
+            timer.clear();
+        };
+    }, []);
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [chat.scrollBottom]);
+
+    return (
+        <Stack
+            ref={chatContainerRef}
+            miw={300}
+            spacing={0}
+            sx={{
+                height:'100%',
+                background: "var(--vscode-sideBar-background)",
+                color: "var(--vscode-editor-foreground)",
+            }}
         >
-          <IconCircleArrowDownFilled size="1.125rem" />
-        </ActionIcon>
-      )}
-      <ScrollArea
-        sx={{
-          height: chat.generating ? height - px("9rem") : height - px("7rem"),
-          padding: 0,
-          margin: 0,
-        }}
-        onScrollPositionChange={onScrollPositionChange}
-        viewportRef={scrollViewport}
-      >
-        <MessageList chatPanelWidth={chatPanelWidth} />
-        <CurrentMessage />
-        {chat.errorMessage && (
-          <Box mb={20}>
-            <Alert
-              styles={{
-                message: { fontSize: "var(--vscode-editor-font-size)" },
-              }}
-              w={chatContainerRect.width}
-              color="gray"
-              variant="filled"
+            <ScrollArea
+                sx={{
+                    height: height - inputAreaHeight - 40,
+                    margin: 0
+                }}
+                onScrollPositionChange={onScrollPositionChange}
+                viewportRef={scrollViewport}
             >
-              {chat.errorMessage}
-            </Alert>
-            {chat.errorMessage.search("Insufficient balance") > -1 && (
-              <Button
-                size="xs"
-                component="a"
-                href={chat.rechargeSite}
-                mt={5}
-                variant="outline"
-                leftIcon={<IconExternalLink size="0.9rem" />}
-              >
+                <MessageList chatPanelWidth={chatPanelWidth} />
+                {chat.errorMessage && (
+                    <Box sx={{
+                        margin:'0 10px 40px 10px'                      
+                    }}>
+                        <Alert
+                            styles={{
+                                message: { fontSize: "var(--vscode-editor-font-size)" },
+                            }}
+                            color="gray"
+                            variant="filled"
+                        >
+                            {chat.errorMessage}
+                        </Alert>
+                        {chat.errorMessage.search("Insufficient balance") > -1 && (
+                            <Button
+                                size="xs"
+                                component="a"
+                                href={chat.rechargeSite}
+                                mt={5}
+                                variant="outline"
+                                leftIcon={<IconExternalLink size="0.9rem" />}
+                            >
                 Open official website to recharge.
-              </Button>
-            )}
-          </Box>
-        )}
-      </ScrollArea>
-      <Stack
-        spacing={0}
-        sx={{ position: "absolute", bottom: 10, width: chatPanelWidth }}
-      >
-        {chat.generating && (
-          <Center mb={5}>
-            <StopButton />
-          </Center>
-        )}
-        {chat.errorMessage && (
-          <Center mb={5}>
-            <RegenerationButton />
-          </Center>
-        )}
-        <InputMessage chatPanelWidth={chatPanelWidth} />
-        <Chip.Group
-          multiple={false}
-          value={chat.chatModel}
-          onChange={(value) => {
-            chat.changeChatModel(value);
-            messageUtil.sendMessage({
-              command: "updateSetting",
-              key1: "devchat",
-              key2: "defaultModel",
-              value: value,
-            });
-          }}
-        >
-          <Group position="left" spacing={5} mt={5}>
-            <Chip size="xs" styles={{ label: chipStyle }} value="gpt-3.5-turbo">
-              GPT-3.5
-            </Chip>
-            <Chip
-              size="xs"
-              styles={{ label: chipStyle }}
-              value="gpt-3.5-turbo-16k"
+                            </Button>
+                        )}
+                    </Box>
+                )}
+                {!chat.isBottom && (
+                    <ActionIcon
+                        onClick={() => {
+                            scrollToBottom();
+                        }}
+                        title="Bottom"
+                        variant="transparent"
+                        sx={{ position: "absolute", bottom: 5, right: 16, zIndex: 1 }}
+                    > 
+                        <IconCircleArrowDownFilled size="1.125rem" />
+                    </ActionIcon>
+                )}
+                {chat.generating && (
+                    <Center sx={{ position: "absolute", bottom: 5, zIndex: 1,width:'100%' }}>
+                        <StopButton />
+                    </Center>
+                )}
+                {chat.errorMessage && (
+                    <Center sx={{ position: "absolute", bottom: 5, zIndex: 1,width:'100%' }}>
+                        <RegenerationButton />
+                    </Center>
+                )}
+            </ScrollArea>
+            <Box 
+                ref={inputAreatRef}
+                sx={{
+                    position:"absolute",
+                    bottom:0,
+                    width:chatPanelWidth,
+                    background: "var(--vscode-sideBar-background)",
+                    boxShadow: "0 0 10px 0 var(--vscode-widget-shadow)",
+                    borderTop:'1px solid #ced4da',
+                }}
             >
-              GPT-3.5-16K
-            </Chip>
-            <Chip size="xs" styles={{ label: chipStyle }} value="gpt-4">
-              GPT-4
-            </Chip>
-			<Chip size="xs" styles={{ label: chipStyle }} value="claude-2">
-			  Claude-2
-            </Chip>
-          </Group>
-        </Chip.Group>
-      </Stack>
-    </Box>
-  );
+                <InputMessage chatPanelWidth={chatPanelWidth} />
+            </Box>
+        </Stack>
+    );
 });
 
 export default chatPanel;
