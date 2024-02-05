9174110805d93e9006a67494f7fbfc41a48bd49e
bobo.yang <bobo.yang@merico.dev>
2024-02-05 11:19:01 +0800

    use TopicEntry as getTopics result

--- src/handler/topicHandler.ts
+++ src/handler/topicHandler.ts
@@ -2,14 +2,15 @@ import * as vscode from 'vscode';
 import { TopicManager } from '../topic/topicManager';
 import { regInMessage, regOutMessage } from '../util/reg_messages';
 import { MessageHandler } from './messageHandler';
+import DevChat, { TopicEntry } from '../toolwrapper/devchat';
 
 // 注册获取当前topic列表的命令
 regInMessage({ command: 'getTopics' });
 regOutMessage({ command: 'getTopics', topics: [] });
 export async function getTopics(message: any, panel: vscode.WebviewPanel | vscode.WebviewView): Promise<void> {
-	await TopicManager.getInstance().loadTopics();
-    const topics = TopicManager.getInstance().getTopicList();
-    MessageHandler.sendMessage(panel, { command: 'getTopics', topics });
+    const devChat = new DevChat();
+	const topicEntries: TopicEntry[] = await devChat.topics();
+    MessageHandler.sendMessage(panel, { command: 'getTopics', topicEntries });
 }
 
 // 注册删除topic的命令
