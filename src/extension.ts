4907715d56f5843e239c57ab31388f0715e06239
bobo.yang <bobo.yang@merico.dev>
2024-04-08 22:10:56 +0800

    Add code complete functionality

--- src/extension.ts
+++ src/extension.ts
@@ -31,6 +31,7 @@ import { startRpcServer } from './ide_services/services';
 import { registerCodeLensProvider } from './panel/codeLens';
 import { stopDevChatBase } from './handler/sendMessageBase';
 import { DevChatConfig } from './util/config';
+import { InlineCompletionProvider, registerCodeCompleteCallbackCommand } from "./contributes/codecomplete/codecomplete";
 
 
 async function migrateConfig() {
