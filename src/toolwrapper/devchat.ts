be6f8cc88823e41824bb95a8ef31a0ee4328ed71
bobo.yang <bobo.yang@merico.dev>
2024-05-08 15:35:32 +0800

    Update DevChat configuration and GUI submodule commit hash

--- src/toolwrapper/devchat.ts
+++ src/toolwrapper/devchat.ts
@@ -258,7 +258,8 @@ class DevChat {
 				"OPENAI_API_KEY": llmModelData.api_key.trim(),
 				"DEVCHAT_UNIT_TESTS_USE_USER_MODEL": 1,
 				// eslint-disable-next-line @typescript-eslint/naming-convention
-				...llmModelData.api_base? { "OPENAI_API_BASE": llmModelData.api_base, "OPENAI_BASE_URL": llmModelData.api_base } : {}
+				...llmModelData.api_base? { "OPENAI_API_BASE": llmModelData.api_base, "OPENAI_BASE_URL": llmModelData.api_base } : {},
+				"DEVCHAT_PROXY": DevChatConfig.getInstance().get('DEVCHAT_PROXY') || "",
 			};
 
 			// build process options
