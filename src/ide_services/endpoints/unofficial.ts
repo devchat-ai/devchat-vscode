dad1f10513f845493ee1c3f10be541a4d841d82c
bobo.yang <bobo.yang@merico.dev>
2024-07-16 07:57:13 +0800

    chore: Remove unused endpoint "/get_symbol_defines_in_selected_code" and related code

--- src/ide_services/endpoints/unofficial.ts
+++ src/ide_services/endpoints/unofficial.ts
@@ -1,6 +1,4 @@
-import * as vscode from "vscode";
 import { applyCodeWithDiff, applyEditCodeWithDiff } from "../../handler/diffHandler";
-import { getSymbolDefines } from "../../context/contextRefDefs";
 
 
 export namespace UnofficialEndpoints {
@@ -11,16 +9,6 @@ export namespace UnofficialEndpoints {
             applyCodeWithDiff({ fileName: filepath, content: content }, undefined);
         }
         return true;
-    }
-
-    export async function getSymbolDefinesInSelectedCode() {
-        // find needed symbol defines in current editor document
-        // return value is a list of symbol defines
-        // each define has three fileds:
-        // path: file path contain that symbol define
-        // startLine: start line in that file
-        // content: source code for symbol define
-        return getSymbolDefines();
     }
 
 	export async function runCode(code: string) {
