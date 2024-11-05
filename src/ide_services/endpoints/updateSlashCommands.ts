a998b11f1ee988f6031f13a9a04ee74d58c87a07
bobo.yang <bobo.yang@merico.dev>
2024-11-05 11:27:30 +0800

    add log for update slash commands

--- src/ide_services/endpoints/updateSlashCommands.ts
+++ src/ide_services/endpoints/updateSlashCommands.ts
@@ -1,7 +1,9 @@
+import { logger } from '../../util/logger';
 import * as vscode from 'vscode';
 
 
 export async function updateSlashCommands() {
+    logger.channel()?.debug('Updating slash commands...');
     vscode.commands.executeCommand('DevChat.InstallCommands');
     return true;
 }
