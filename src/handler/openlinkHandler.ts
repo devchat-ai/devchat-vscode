31333e25e7bc4587e69b125d1407f7992ba247e3
bobo.yang <bobo.yang@merico.dev>
2025-02-17 16:31:46 +0800

    refactor: Remove unused imports in openlinkHandler
    - Remove unnecessary fs, path, yaml imports
    - Remove unused MessageHandler, DevChatConfig imports
    - Remove logger import while keeping required regInMessage

--- src/handler/openlinkHandler.ts
+++ src/handler/openlinkHandler.ts
@@ -2,15 +2,8 @@
 Commands for handling configuration read and write
 */
 
-import * as fs from 'fs';
-import * as path from 'path';
 import * as vscode from 'vscode';
-import yaml from 'yaml';
-import { regInMessage, regOutMessage } from '../util/reg_messages';
-import { MessageHandler } from './messageHandler';
-import { DevChatConfig } from '../util/config';
-import { logger } from '../util/logger';
-
+import { regInMessage } from '../util/reg_messages';
 
 regInMessage({command: 'openLink', url: 'http://...'}); // when key is "", it will rewrite all config values
 export async function openLink(message: any, panel: vscode.WebviewPanel|vscode.WebviewView): Promise<void> {
